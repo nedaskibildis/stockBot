40fa739be90bbd0a4153a11dd744543cfded1818
Nedas Kibildis <nedas11@icloud.com>
2024-03-15 01:32:08 -0400

    Made some changes to database file, added function to get most recent stock bought by a congress member so that we can have that as a command for the bot

--- database.py
+++ database.py
@@ -1,33 +1,64 @@
 import sqlite3
 from scraper import scrapeStocks
-con = sqlite3.connect("")
-cursor = con.cursor()
 
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS  StockTransactions (
-    Politician TEXT,
-    Stocks TEXT,
-    BuyOrSell TEXT,
-    SizeOf INTEGER,
-    PriceOfStock REAL,
-    DateBought TEXT,
-    DatePublished TEXT
-    )
-""")
+def setupDb():
+    con = sqlite3.connect("discordBot.db")
+    cursor = con.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS  StockTransactions (
+        Politician TEXT,
+        Stocks TEXT,
+        BuyOrSell TEXT,
+        SizeOf INTEGER,
+        PriceOfStock REAL,
+        DateBought TEXT,
+        DatePublished TEXT
+        )
+    """)
+
+def pullStocks():
+    con = sqlite3.connect("discordBot.db")
+    cursor = con.cursor()
+    stockTransaction = scrapeStocks()
+    for stock in stockTransaction:
+        checkExisting(stock)
+        cursor.execute("INSERT INTO StockTransactions (politician, Stocks, BuyOrSell, SizeOf, PriceOfStock, DateBought, DatePublished) VALUES (:politician, :stocks, :buyOrSell, :sizeOf, :priceOfStock, :dateBought, :datePublished)", stock)
+
+    con.commit()
+    cursor.close()
+    con.close()
 
 def checkExisting(transaction):
+    con = sqlite3.connect("discordBot.db")
+    cursor = con.cursor()
     cursor.execute("""
         SELECT COUNT(*) FROM StockTransactions 
         WHERE Politician=:politician AND Stocks=:stocks AND BuyOrSell=:buyOrSell AND SizeOf=:sizeOf AND PriceOfStock=:priceOfStock AND DateBought=:dateBought AND DatePublished=:datePublished
     """, transaction)
     count = cursor.fetchone()[0]
+    cursor.close()
+    con.close()
     return count > 0
 
-stockTransaction = scrapeStocks()
-for stock in stockTransaction:
-    checkExisting(stock)
-    cursor.execute("INSERT INTO StockTransactions (politician, Stocks, BuyOrSell, SizeOf, PriceOfStock, DateBought, DatePublished) VALUES (:politician, :stocks, :buyOrSell, :sizeOf, :priceOfStock, :dateBought, :datePublished)", stock)
+def printDatabase():
+    con = sqlite3.connect("discordBot.db")
+    cursor = con.cursor()
+    cursor.execute("""
+    SELECT * FROM StockTransactions
+    """)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    
+    cursor.close()
+    con.close()
 
-con.commit()
-cursor.close()
-con.close()
+def getRecent():
+    con = sqlite3.connect("discordBot.db")
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM StockTransactions ORDER BY dateBought DESC LIMIT 1;")
+    row = cursor.fetchone()
+    return row
+
+printDatabase()
+print(getRecent())
