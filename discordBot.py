056755627847b286909d04e39b9e398279a0be69
Nedas Kibildis <nedas11@icloud.com>
2024-03-18 19:41:03 -0400

    Basic bot running

--- discordBot.py
+++ discordBot.py
@@ -1,3 +1,4 @@
+from pydoc import cli
 from typing import Final
 import os
 from dotenv import load_dotenv
@@ -24,6 +25,29 @@ async def sendMessage(message: Message, userMessage: str) -> None:
 
     try: 
         response: str = responses.get_response(userMessage)
-        await message.author.send(response) if isPrivate else message.channel.send(response)
+        await message.author.send(response) if isPrivate else await message.channel.send(response)
     except Exception as e:
         print(e)
+
+# STEP 3: HANDLING THE STARTUP FOR THE BOT
+@client.event
+async def on_ready() -> None:
+    print(f'{client.user} is now running')
+
+@client.event
+async def on_message(message: Message) -> None:
+    if message.author == client.user:
+        return
+    
+    username: str = str(message.author)
+    userMessage: str = message.content
+    channel: str = str(message.channel)
+
+    print(f'[{channel}] {username}: "{userMessage}"')
+    await sendMessage(message, userMessage)
+
+def main() -> None:
+    client.run(token=TOKEN)
+
+if __name__ == '__main__':
+    main()
