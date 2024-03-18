3d0f66c67518d1bf81b7759157c7a7858edee001
Nedas Kibildis <nedas11@icloud.com>
2024-03-18 19:34:44 -0400

    added basic response system

--- responses.py
+++ responses.py
@@ -1,2 +1,13 @@
+from random import choice, randint
+
 def get_response(userInput: str) -> str:
-    raise NotImplementedError
+    lowered: str = userInput.lower()
+
+    if lowered == '':
+        return "Well, You're awfully Silent"
+    elif 'hello' in lowered:
+        return 'Hello There!'
+    else:
+        return choice(['I do not understand...',
+                        'What are you talking about? ',
+                        ' Do you mind rephrasing that'])
