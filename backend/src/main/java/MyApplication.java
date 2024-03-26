011aa83e17986bd3f3dfb697594b46034f7c238a
Nedas Kibildis <nedas11@icloud.com>
2024-03-25 23:04:48 -0400

    Added scraper-selenium network and changed Spring Dockerfile to work

--- backend/src/main/java/MyApplication.java
+++ backend/src/main/java/MyApplication.java
@@ -1,5 +1,10 @@
 package com.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @SpringBootApplication
 public class MyApplication {
