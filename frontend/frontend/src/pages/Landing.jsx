157e076256a171d2fab117975bd421a731b14712
Nedas Kibildis <nedas11@icloud.com>
2024-04-03 01:09:08 -0400

    Finished randomization of colors

--- frontend/frontend/src/pages/Landing.jsx
+++ frontend/frontend/src/pages/Landing.jsx
@@ -2,18 +2,18 @@ import HomeButton from "../components/HomeButton";
 import MovingGradient from "../components/MovingGradient";
 
 export default function Landing() {
-     // Empty dependency array, runs once on mount
 
+    const colorChoices =[ "#b5179e", "#7209b7", "#560bad", "#480ca8", "#3f37c9", "#4361ee"]
 
     return (
         <div className="relative h-screen flex flex-col justify-center items-center bg-[#3a0ca3]" style={{overflow:'hidden'}}>
-            <MovingGradient color={"#560bad"}/>
-            <MovingGradient color={"#560bad"}/>
-            <MovingGradient color={"#560bad"}/>
-            <MovingGradient color={"#b5179e"} />
-            <MovingGradient color={"#b5179e"}/>
-            <MovingGradient color={"#4361ee"}/>
-            <MovingGradient color={"#4361ee"} />
+            <MovingGradient color={colorChoices[Math.floor(Math.random() * colorChoices.length)]}/>
+            <MovingGradient color={colorChoices[Math.floor(Math.random() * colorChoices.length)]}/>
+            <MovingGradient color={colorChoices[Math.floor(Math.random() * colorChoices.length)]}/>
+            <MovingGradient color={colorChoices[Math.floor(Math.random() * colorChoices.length)]} />
+            <MovingGradient color={colorChoices[Math.floor(Math.random() * colorChoices.length)]}/>
+            <MovingGradient color={colorChoices[Math.floor(Math.random() * colorChoices.length)]}/>
+            <MovingGradient color={colorChoices[Math.floor(Math.random() * colorChoices.length)]} />
             <h1 className="text-6xl z-10 font-bold text-mainWhite mb-3">Welcome To The Capital Trading Tracker</h1>
             <h3 className="text-3xl z-10 font-medium  text-mainWhite mb-3"> The Easiest Way To Keep Track Of Congress Members Stock Purchases</h3>
             <p className="text-lg z-10 text-mainWhite mb-3">Select On Of Our Options Below To Get Started</p>
